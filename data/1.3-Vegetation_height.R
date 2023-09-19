6b7510e228d3e7c78ac4be26475752c02691e94c
AlbyDR <albyduarte@gmail.com>
2023-09-19 13:38:48 +0200

    updated

--- data/1.3-Vegetation_height.R
+++ data/1.3-Vegetation_height.R
@@ -1,211 +1,91 @@
-library(rSCOPE)
 library(raster)
-library(sf)
-library(exactextractr)
-library(ggplot2)
-library(ggspatial)
-library(gghighlight)
-library(lubridate)
+library(rSCOPE)
+library(curl)
+library(terra)
 library(tidyverse)
-#############################################################
-############ polygons BERLIN ENVIRONMENT ATLAS ##############
-#############################################################
-# green volume map
-Green.blocks <- get_fisbroker_map(url = "https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_05_09_gruenvol2010")
-Green.street <- get_fisbroker_map(url = "https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/wfs_05_09_gruenvol2010_str")
 
-names(Green.blocks)
-names(Green.street)
+# Global Canopy Height 2020
 
-str(filter(Green.blocks, FLALLE==160724.4)) 
-####################################################################
-#$ gml_id    : chr "s_05_09_gruenvol2010.0700231951000000" #key TUCC
-#$ FLALLE    : num 160724  = area
-#$ VEGHOE    : num 9.4     = Mean height of vegetation in relation to the area covered with vegetation [m]
-#$ VEGPROZ   : num 30.4    = Area covered with vegetation in relation to the total area [%]
-#$ VEGVOLA   : int 461279  = Green volume [m³]
-#$ VEGVOL    : num 2.87    = Green volume number [m³ / m²]
-#$ FLUBEB    : num 87791   = Area size of the above-ground undeveloped area [m²]
-#$ VEGHOEUBEB: num 9.4     = Mean height of the vegetation in relation to the area of the undeveloped area covered with vegetation [m]
-#$ VEGPROUBEB: num 53.9    = Area covered with vegetation in relation to the undeveloped area [%]
-#$ VEGVOLAUBE: int 446854  = Green volume of the undeveloped area [m³]
-#$ VEGVOLUBEB: num 5.09    = Number of green volumes related to the area of the undeveloped area covered with vegetation [m³ / m²]
-################################################################
+##################################################################################
+# get GCH image link
+##################################################################################
+# run to get data
+get_GCH(city_border = NA,
+        i_city = "city_name_1",
+        N_S_degree = 52.51228,
+        W_E_degree = 13.32786,
+        dest_file = "Data-Modelling/Canopy_Height/")
 
-# combine both
-Green_vol <- rbind(Green.blocks[c(-2,-7)], Green.street[-2])
-Green_vol <- sf::st_cast(Green_vol)
-
-ggplot() + 
-  geom_sf(data = Green_vol, aes(fill = veghoe), colour = "transparent") +
-  geom_sf(data = berlin.sf, fill = "transparent", size = 1.2, color = "black") +
-  scale_fill_gradientn(breaks = seq(0, 30, 5), limits = c(0,31),
-                       colors = rev(terrain.colors(10)), name = "", na.value = 0,
-                       guide = guide_colorbar(direction = "horizontal",
-                                              label.position = "bottom",
-                                              label.vjust = -2,
-                                              frame.colour = "black",
-                                              frame.linewidth = 0.5,
-                                              frame.linetype = 1,
-                                              barwidth=35, barheight=1.5, nbin=10,
-                                              label.theme=element_text(angle=0,size=16))) +
-  theme(axis.line=element_blank(),
-        axis.text.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks=element_blank(),
-        legend.position="bottom",
-        legend.spacing.y = unit(-0.4, "lines"),
-        legend.box.spacing = unit(-1.5, "lines"),
-        panel.grid.major = element_blank(), 
-        panel.background = element_rect("white"))
-
-plot(Green.blocks["veghoe"], border = "transparent")
-plot(Green.street["veghoe"], border = "transparent")
-plot(Green_vol["veghoe"], border = "transparent")
-
-summary(Green_vol["veghoe"])
-
-# convert to a raster
-krg_grid_10m <- raster::disaggregate(krg_grid, fact = 100)
-veg_height_raster <- fasterize::fasterize(Green_vol, field = "veghoe", 
-                                          krg_grid_10m, fun = "max")
-plot(veg_height_raster)
-summary(veg_height_raster)
-
-# aggregate to 1km grid
-veg_height_r_1km <- raster::aggregate(veg_height_raster, fact = 100)
-plot(veg_height_r_1km)
-summary(veg_height_r_1km)
-
-# resample to 1km grid
-VH_1km <- raster::resample(veg_height_r_1km, krg_grid, "ngb")
-plot(VH_1km)
-summary(VH_1km)
-
-names(VH_1km) <- "VH_1km"
-
-# raster::writeRaster(VH_1km, "hc_VH_1km", overwrite = TRUE)
-# VH_1km <- raster::raster("hc_VH_1km")
+# for namy location (obj_locations_cities is a list of city coordination)
+lapply(1:length(obj_locations_cities), FUN = function(i) 
+  get_GCH(city_border = obj_locations_cities,
+          i_city = i,
+          N_S_degree = obj_locations_cities[[i]]$latlon$points[[1]][[1]][2],
+          W_E_degree = obj_locations_cities[[i]]$latlon$points[[1]][[1]][1],
+          dest_file = "D:/Data-Modelling/Canopy_Height/"))
 
 
-####################################################################################
-# vegetation fraction (block and streets)
-####################################################################################
-veg_fraction <- Green_vol["vegproz"]
-names(veg_fraction)[1] <- "veg_fraction"
-veg_fraction$vf <- Green_vol$vegproz/100
+##################################################################################
+# open the GCH downloaded
+#######################################################################################
 
-summary(veg_fraction)
-plot(veg_fraction["veg_fraction"], border = "transparent")
+##################################################################################
+# run to open CGH
+##################################################################################
+list_GCH <- lapply(1:13, FUN = function(i)
+  list.files("D:/Data-Modelling/Canopy_Height/downloaded/",
+             pattern = names(obj_locations_cities)[i]))
 
-#saveRDS(veg_fraction, "veg_fraction")
-####################################################################################
+GCH_Amsterdam <- open_GCH(crop_area = obj_locations_cities[[12]]$latlon$buffer_dist,
+                          list_GCH = list_GCH[[12]], 
+                          city_name = names(obj_locations_cities)[12],
+                          patch_file = "D:/Data-Modelling/Canopy_Height/downloaded/")
 
+plot(GCH_Amsterdam[[i]])
+plot(obj_locations_cities[[12]]$latlon$border, add=T)
+plot(obj_locations_cities[[12]]$latlon$points, add=T)
 
-############################################################################
-# LULC Copernicus Urban Atlas 2018
-LCLU_Uatlas <- sf::st_read("C:/Users/.../Documents/UWI/R_SCOPE/DE001L1_BERLIN_UA2018_v013/Data/DE001L1_BERLIN_UA2018_v013.gpkg")
-LCLU_Uatlas <- sf::st_transform(LCLU_Uatlas, crs = sf::st_crs(Berlin_buffer_utm))
+terra::writeRaster(GCH_Amsterdam, "D:/Research topics/Data-Modelling/EUcities/Canopy_Height/GCH_Amsterdam.tif")
+#GCH_Amsterdam <- terra::rast("D:/Research topics/Data-Modelling/EUcities/Canopy_height/GCH_Amsterdam.tif")
 
-# crop to Berlin Area
-LCLU_berlin <- sf::st_crop(LCLU_Uatlas, Berlin_buffer_utm)
-
-unique(LCLU_Uatlas_berlinb$class_2018)
-unique(LCLU_Uatlas_berlinb$code_2018)
-
-LCLU_b <- LCLU_berlin["class_2018"]
-LCLU_b$classII <- LCLU_berlin$class_2018
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Fast transit roads and associated land"] <- "Roads"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Other roads and associated land"] <- "Roads"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Railways and associated land"] <- "Roads"
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Arable land (annual crops)"] <- "Crops and pastures"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Complex and mixed cultivation patterns"] <- "Crops and pastures"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Pastures"] <- "Crops and pastures"
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Construction sites"] <- "Other articifial areas"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Isolated structures"] <-  "Other articifial areas"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Land without current use"] <-  "Other articifial areas"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Mineral extraction and dump sites"] <-  "Other articifial areas"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Airports"] <- "Other articifial areas"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Port areas"] <- "Other articifial areas"
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Sports and leisure facilities"] <- "Sports and leisure"
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Industrial, commercial, public, military and private units"] <- "Industrial-commercial-public"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Continuous urban fabric (S.L. : > 80%)"] <-  "Urban fabric (>80%)"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Discontinuous dense urban fabric (S.L. : 50% -  80%)"] <-  "Urban fabric (50%-80%)"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Discontinuous medium density urban fabric (S.L. : 30% - 50%)"] <-  "Urban fabric (30%-50%)"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Discontinuous low density urban fabric (S.L. : 10% - 30%)"] <- "Urban fabric (10%-30%)"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Discontinuous very low density urban fabric (S.L. : < 10%)"] <- "Urban fabric (10%)"
-
-LCLU_b$classII[LCLU_berlin$class_2018 == "Herbaceous vegetation associations (natural grassland, moors...)"] <- "Herbaceous vegetation"
-LCLU_b$classII[LCLU_berlin$class_2018 == "Open spaces with little or no vegetation (beaches, dunes, bare rocks, glaciers)"] <- "Natural, no vegetation"
-
-unique(LCLU_b$classII)
-t(table(LCLU_berlin$class_2018))
-
-paletteLULC <- c(
-  "Urban fabric (>80%)" =  "#800000",
-  "Urban fabric (50%-80%)"  =  "#BE0000",
-  "Urban fabric (30%-50%)" =  "#FE4040",
-  "Urban fabric (10%-30%)" =  "#FE8081",
-  "Urban fabric (10%)" =  "#FFBFBF",
-  "Industrial-commercial-public" =  "#CC4DF2",
-  "Other articifial areas" =  "#E6CCE7",
-  "Roads"=  "#B3B3B3",
-  "Natural, no vegetation" ="#CDFFCC",
-  "Sports and leisure" =  "#AED2A4",
-  "Green urban areas" = "#8CDC00",
-  "Herbaceous vegetation" ="#CCF24D",
-  "Forests" = "#008C00",
-  "Crops and pastures" = "#FFFFA7",
-  "Water"  = "#80F3E5",
-  "Wetlands" = "#A6A6FE")
-
-LabelsLULC <- c(
-  "Urban fabric (>80%)",
-  "Urban fabric (50%-80%)",
-  "Urban fabric (30%-50%)",
-  "Urban fabric (10%-30%)",
-  "Urban fabric (10%)",
-  "Industrial-commercial-public",
-  "Other articifial areas",
-  "Roads",
-  "Natural, no vegetation",
-  "Sports and leisure",
-  "Green urban areas",
-  "Herbaceous vegetation",
-  "Forests",
-  "Crops and pastures",
-  "Water",
-  "Wetlands")
-
-ggplot(LCLU_b) +
-  geom_sf(aes(fill = classII), colour = NA) +
-  geom_sf(data = Berlin_border_utm, fill = "transparent", size = 1.2, color = "black") +
-  scale_fill_manual(values = paletteLULC,
-                    labels = LabelsLULC,
-                    breaks = LabelsLULC,
-                    name = "LU/LC")
-####################################################################################
+GCH_Amsterdam_utm <- terra::project(GCH_Amsterdam, prj_utm)
+terra::writeRaster(GCH_Amsterdam_utm, "D:/Research topics/Data-Modelling/EUcities/Canopy_height/GCH_Amsterdam_utm.tif")
 
 
-# Biotype
-###################################################################################
-Biotope <- get_fisbroker_map(url = "https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_fb_berlinbtk")
+# Amsterdam by hand
 
-# subset some forest types to make it plot faster
-Forest_biotope <- Biotope[Biotope$BT_NAME %in% unique(filter(Biotope, grepl('forst', BT_NAME, fixed = TRUE))$BT_NAME), ]
-plot(Forest_biotope["BT_NAME"])
+site = "Amsterdam"
 
-Pine <- Biotope[Biotope$BT_NAME %in% unique(filter(Biotope, grepl('Kiefern', BT_NAME, fixed = TRUE))$BT_NAME), ]
-Beech <- Biotope[Biotope$BT_NAME %in% unique(filter(Biotope, grepl('Buche', BT_NAME, fixed = TRUE))$BT_NAME), ]
-Oak <- Biotope[Biotope$BT_NAME %in% unique(filter(Biotope, grepl('Eiche', BT_NAME, fixed = TRUE))$BT_NAME), ]
+N_S = "N"
+N_S_degree = 52
+W_E = "E"
+W_E_degree = 4
 
-Pine <- st_collection_extract(Pine)
-Oak <- st_collection_extract(Oak)
-Beech <- st_collection_extract(Beech)
-###################################################################################
+degree_N_S <- seq(0, 180, 3)[which(data.table::between(N_S_degree, seq(0, 177, 3), seq(3, 180, 3)))]
+degree_W_E <- seq(0, 180, 3)[which(data.table::between(W_E_degree, seq(0, 177, 3), seq(3, 180, 3)))]
 
+degree_W_E <- ifelse(degree_W_E < 100, paste0("0", degree_W_E), degree_W_E)
+
+url <- paste0("https://share.phys.ethz.ch/~pf/nlangdata/ETH_GlobalCanopyHeight_10m_2020_version1/3deg_cogs/ETH_GlobalCanopyHeight_10m_2020_",
+              N_S, degree_N_S, W_E, degree_W_E,"_Map.tif")
+
+download.file("https://share.phys.ethz.ch/~pf/nlangdata/ETH_GlobalCanopyHeight_10m_2020_version1/3deg_cogs/ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif",
+              destfile = "D:/Data-Modelling/Canopy_Height/ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif", 
+              method="curl")
+
+canopy_height_Amsterdam <- raster::raster("D:/Data-Modelling/Canopy_Height/ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif")
+plot(canopy_height_Amsterdam)
+
+canopy_height_Amsterdam <- crop(canopy_height_Amsterdam, extent(Amsterdam_border))
+
+plot(canopy_height_Amsterdam)
+plot(Amsterdam_border, color = "transparent", add = TRUE)
+
+terra::writeRaster(canopy_height_Amsterdam, "canopy_height_Amsterdam.tif")
+canopy_height_Amsterdam <- terra::rast("canopy_height_Amsterdam")
+
+# vegetation height
+raster::extract(canopy_height_Amsterdam, Amsterdam_tower)
+summary(unlist(raster::extract(canopy_height_Amsterdam, Amsterdam_tower_buffer)))
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+# 4.00   11.00   13.00   12.91   14.00   19.00   12481
